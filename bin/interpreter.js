20377029df99e4bc5f525d433291209563ca78bd
David H. Friedman <david.h.friedman.nyc@gmail.com>
2019-09-20 10:40:19 -0400

    7 evaluation; tweak semantics of equality and truthiness, remove throws from interpreter visitor

--- bin/interpreter.js
+++ bin/interpreter.js
@@ -22,22 +22,38 @@ class InterpreterError extends Error {
   }
 }
 
+function isTruthy (e) {
+  // The Ruby way: false and nil are falsey all else truthy
+  if (e === null || e === undefined) { return false }
+  else if (e === true || e === false) { return e }
+  else { return true }
+}
+
+function areEqual(a, b) {
+  // TODO: relying on JavaScript coercion. Make semantics more explicit.
+  return a == b
+}
+
 const InterpreterVisitor = {
-  visitLiteral: function (l) { return l.val ? l.val : null },
+  visitLiteral: function (l) { return l.val },
   visitGrouping: function (g) { return g.expr.accept(this) },
   visitUnary: function (u) {
     let e = u.expr.accept(this)
     if (u.operator.type === TokenType.MINUS) {
+      // TODO: type check
       e = -e
     } else if (u.operator.type === TokenType.BANG) {
-      e = !e
+      e = !isTruthy(e)
     } else {
-      throw error(u, "Expected '-' or '!'.")
+      //throw error(u, "Expected '-' or '!'.")
+      e = null
     }
     return e
   },
   visitBinary: function (b) {
     // TODO: later, for short circuiting logical operators, don't evaulate right unless needed
+    // NOTE: operators evaluated in left-to-right order. Do we declare this part of the
+    // semantics of Lox? Or is it an implementation decision not to be relied on?
     let left = b.left.accept(this)
     let right = b.right.accept(this)
     let e
@@ -45,10 +61,10 @@ const InterpreterVisitor = {
     // currently, javascript coerces and everything just "comes out right"
     switch (b.operator.type) {
     case TokenType.EQUAL_EQUAL:
-      e = (left == right)
+      e = areEqual(left, right)
       break
     case TokenType.BANG_EQUAL:
-      e = (left != right)
+      e = !areEqual(left, right)
       break
     case TokenType.LESS:
       e = (left < right)
@@ -75,7 +91,8 @@ const InterpreterVisitor = {
       e = (left / right)
       break
     default:
-      throw error(b, `Expected '==', '!=', '<', '<=', '>', '>=', '+', '-', '*', '/'.`)
+      //throw error(b, `Expected '==', '!=', '<', '<=', '>', '>=', '+', '-', '*', '/'.`)
+      e = null
     }
     return e
   }
